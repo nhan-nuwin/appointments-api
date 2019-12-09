b01379badd2ef6ade04df94febb33f93127a1ceb
Nhan <nhan@codistry.io>
2019-12-09 02:12:55 -0800

    Added v1 to API uri

--- routes/index.js
+++ routes/index.js
@@ -1,57 +1,7 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const apiV1 = require('./v1/index');
 
-/* Get list of all doctors */
-router.get('/doctors', function(req, res, next) {
-  const id = req.query.id;
-
-  if(id) {
-    id = parseInt(id);
-    db.get(id)
-      .then( response => response.json() )
-      .then( data => {
-        const firstName = data[0].first_name;
-        const lastName = data[0].last_name;
-        const obj = {
-          firstName,
-          lastName
-        };
-
-        res.send(obj);
-      })
-      .catch( err => console.log(err));
-  } else {
-    res.send('Not Found');
-  }
-});
-
-/* Insert Doctor's Names */
-router.post('/doctors', function(req, res,next) {
-  let firstName = req.query['first-name'];
-  let lastName = req.query['last-name'];
-
-  if(firstName && lastName) {
-    db.add({firstName, lastName})
-      .then( () => res.send('Success'))
-      .catch( err => console.log(err));
-  }
-});
-
-/* Get list of appointments */
-router.get('/appointments', function(req, res, next) {
-});
-
-/* Delete an existing appointment */
-router.delete('/appointments', function(req, res, next) {
-});
-
-/* Add new appointments */
-router.post('/appoinments', function(req, res, next) {
-});
-
-/* Testing */
-router.get('/test', function(req, res, next) {
-});
-
+router.use('/v1', apiV1);
 
 module.exports = router;
