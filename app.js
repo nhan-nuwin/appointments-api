e10a94bb4f4643dbd02c6b7404da27cb986d96a1
Nhan <nhan@codistry.io>
2019-12-16 21:07:21 -0800

    renamed indexRouter to mainRouter in app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var app = express();
-var indexRouter = require('./routes/mainRouter');
+var mainRouter = require('./routes/mainRouter');
 
 // view engine setup
 /*
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', mainRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
