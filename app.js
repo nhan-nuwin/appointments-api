9fc9cb4ec6ab4a1ec9f3460e58a1d6463d4e96cc
Nhan <nhan@codistry.io>
2019-12-16 23:07:36 -0800

    added winston logger

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+//var logger = require('morgan');
+var winston = require('winston');
 
 var app = express();
 var mainRouter = require('./routes/mainRouter');
@@ -13,7 +14,21 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 */
 
-app.use(logger('dev'));
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    //
+    // - Write all logs with level `error` and below to `error.log`
+    // - Write all logs with level `info` and below to `combined.log`
+    //
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' })
+  ]
+});
+
+//app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
