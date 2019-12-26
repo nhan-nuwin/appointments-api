6bb07a13027fabab5df968db5e96ad2f1301c955
Nhan <nhan@codistry.io>
2019-12-26 01:46:16 -0800

    Cleaned up code, adjusted SQL statements so numRows can be cleaner clean cb arg names because we're sure won't be refering to the parent args.

--- routes/dbSchema.js
+++ routes/dbSchema.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const db = require('../db');
 
 // Create DB
@@ -40,3 +41,43 @@ db.query(sql, (err, results, fields) => {
 
 });
 */
+=======
+const db = require('./db');
+
+// Create DB
+let sql = 
+`create table doctors(
+  id int auto_increment,
+  first_name varchar(100),
+  last_name varchar(100),
+  primary key(id)
+);
+
+create table patients(
+  id int auto_increment,
+  first_name varchar(100),
+  last_name varchar(100),
+  primary key(id)
+);
+
+create table appointments(
+  id int auto_increment,
+  date datetime,
+  created timestamp,
+  patient int,
+  doctor int,
+  visit_type varchar(300),
+  primary key(id)
+);`;
+
+/* run command */
+/*
+db.query(sql, (err, results, fields) => {
+  if(err) {
+    console.log(err);
+  }
+
+  console.log(results);
+});
+*/
+>>>>>>> Cleaned up code,
