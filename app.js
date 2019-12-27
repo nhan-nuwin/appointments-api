65db02530f7383c73723ad931f14e475a880f0aa
Nhan <nhan@codistry.io>
2019-12-26 19:20:11 -0800

    enabled cors

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var winston = require('winston');
+var cors = require('cors');
 
 var app = express();
 var mainRouter = require('./routes/mainRouter');
@@ -20,6 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cors());
 app.use('/', mainRouter);
 
 // catch 404 and forward to error handler
